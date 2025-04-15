0407f251e2ff65c654937cc10b25ef017f962ba1
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-04-15 11:14:20 +0530

    added basic structure

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+chmod u+x hooks/pre-commit
 cp hooks/pre-commit .git/hooks/
-chmod +x hooks/pre-commit
+chmod u+x .git/hooks/pre-commit
 
+chmod u+x hooks/pre-push
 cp hooks/pre-push .git/hooks/
-chmod +x hooks/pre-push
+chmod u+x .git/hooks/pre-push
 
